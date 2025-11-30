1191f542e4beba41ac10c5f28b9cb1febf361aff
Becky Kalumu <beckykalumu8@gmail.com>
2025-11-30 22:22:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+let burgers = ['Hamburger','Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger() {
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger)
+}
+
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger)
+}
+
+// Function to change featured drink
+function changeFeaturedDrink() {
+    featureddrink = 'The JavaShake';
+}
+
+// Test the functions
+addBurger();
+changeFeaturedDrink();
+
